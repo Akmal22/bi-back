397b88d02cf8d074afa503a8dbbf7ad062bd6783
Akmal <Akmal9433@gmail.com>
2025-10-11 23:03:08 +0500

    Initial commit

---
+++ src/main/java/kz/bi/BiBackApplication.java
@@ -0,0 +1,13 @@
+package kz.bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/bi/BiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
